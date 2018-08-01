f6b43a5f5582ed534d44400597df2823d7df8614
Abhijeet <Abhijeet@Abhijeet-PC>
2018-08-01 17:08:01 +0530

    1 Aug 2018 Base Project

---
+++ mavenI/src/main/java/mavenI/mavenI/App.java
@@ -0,0 +1,14 @@
+package mavenI.mavenI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to Git World!" );
+    }
+}
