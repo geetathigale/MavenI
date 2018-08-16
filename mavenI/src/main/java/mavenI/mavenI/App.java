436233f91e84a8a074da3fb1cf7f721f6c368006
Abhijeet <Abhijeet@Abhijeet-PC>
2018-08-16 16:59:20 +0530

    change 11

--- mavenI/src/main/java/mavenI/mavenI/App.java
+++ mavenI/src/main/java/mavenI/mavenI/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println( "Changes done is branch1" );
         System.out.println( "Changes done for github public repo." );
         System.out.println( "Change 2 done for github public repo." );
-        System.out.println( "Change 10 done  to test maven" );
+        System.out.println( "Change 11 done  to test maven" );
       
     }
 }
